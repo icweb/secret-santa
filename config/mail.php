21908fc2accd0bb8893a1b8707845c1a6fcf5324
Ian Conway <ianconway@protonmail.com>
2019-11-23 20:22:29 -0500

    Updated mail settings

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@mail.agaysecretsanta.com'),
+        'name' => env('MAIL_FROM_NAME', 'A Gay Secret Santa'),
     ],
 
     /*
