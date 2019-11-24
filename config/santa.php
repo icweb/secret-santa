da6eafb9ae9ecae1ee73eb8b0bd26d3441f69fed
Ian Conway <ianconway@protonmail.com>
2019-11-23 20:10:42 -0500

    Testing old date

--- config/santa.php
+++ config/santa.php
@@ -6,7 +6,7 @@ return [
 
         'deadline' => \Carbon\Carbon::parse('2019-12-10 23:59:59'),
 
-        'pulled' => \Carbon\Carbon::parse('2019-12-11 00:00:00'),
+        'pulled' => \Carbon\Carbon::parse('2019-11-22 20:10:00'),
 
     ]
 
